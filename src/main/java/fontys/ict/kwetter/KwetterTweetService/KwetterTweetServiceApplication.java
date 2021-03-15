2c6f361fd4cd76588006739d2962ba9e215960b8
i383656 <s.westerburger@student.fontys.nl>
2021-03-15 10:05:50 +0100

    first commit

---
+++ src/main/java/fontys/ict/kwetter/KwetterTweetService/KwetterTweetServiceApplication.java
@@ -0,0 +1,13 @@
+package fontys.ict.kwetter.KwetterTweetService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwetterTweetServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KwetterTweetServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fontys/ict/kwetter/KwetterTweetService/KwetterTweetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fontys.ict.kwetter.KwetterTweetService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KwetterTweetServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
