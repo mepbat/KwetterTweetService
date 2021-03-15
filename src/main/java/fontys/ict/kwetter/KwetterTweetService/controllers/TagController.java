9db103feb322b785904b584417efd6b4aceb4fd0
i383656 <s.westerburger@student.fontys.nl>
2021-03-15 12:02:06 +0100

    added @restcontroller for TagController

--- src/main/java/fontys/ict/kwetter/KwetterTweetService/controllers/TagController.java
+++ src/main/java/fontys/ict/kwetter/KwetterTweetService/controllers/TagController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@RestController
+@RequestMapping(value = "/tag")
 public class TagController {
     private final TagRepository tagRepository;
 
